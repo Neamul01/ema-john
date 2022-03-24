aa6880d1331682ebfdd59540a5e1c72b5a3892d7
Neamul01 <neamulhaque2002@gmail.com>
2022-03-24 23:55:57 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello this is testing</h2>
+      <Header></Header>
     </div>
   );
 }
